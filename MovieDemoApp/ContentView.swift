6a54fa291a9a3c3ec49b6bda5978b3182cf7fc83
Liang0 <tenghengliang@hotmail.com>
2023-10-07 00:00:28 +0800

    git commit

---
+++ MovieDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieDemoApp
+//
+//  Created by Heng Liang Teng on 06/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!1")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieDemoApp/MovieDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDemoAppApp.swift
+//  MovieDemoApp
+//
+//  Created by Heng Liang Teng on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
